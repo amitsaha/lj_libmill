67b136cf78f81c5b898b66442e8e686e2e3acce3
Amit Saha <amitsaha.in@gmail.com>
2016-04-03 12:21:58 +1000

    Choosing among channels demo

--- listing4/listing4.c
+++ listing4/listing4.c
@@ -1,20 +1,48 @@
-# include <stdio.h>
 # include <libmill.h>
-# include <string.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 
-coroutine void f(int index)
-{
-  msleep(now() + rand() % 50 );
-  printf("Worker %d\n", index);  
+struct data {
+  char *url;
+  char *data;
+};
+
+coroutine void f(chan work, chan result, chan error) {
+  struct data d;
+  char *url = chr(work, char*);
+  d.url = url;
+  /* Simulate 70% success and 30% failure scenario*/
+  double r = (double) rand()/RAND_MAX;
+  if (r < 0.71) {
+    // Successful scenario
+    d.data = "Data at the URL";
+    chs(result, struct data, d);
+  } else {
+    // Unsuccessful scenario
+    d.data = "Error retreiving data";
+    chs(error, struct data, d);
+  }
 }
 
-int main(int argc, char **argv)
-{
-  gotrace(1);
-  for(int i=1;i<=10; i++) {
-    go(f(i));
+int main(int argc, char **argv) {
+  chan work = chmake(char*, 0);
+  chan result = chmake(struct data, 0);
+  chan error = chmake(struct data, 0);
+
+  for(int i=1; i<argc; i++) {
+    go(f(work, result, error));
+    chs(work, char*, argv[i]);
   }
-  msleep(now() + 60);
+
+  for(int i=1; i<argc; i++) {
+    choose {
+    in(result, struct data, value):
+      printf("Processed URL: %s, Result: %s\n", value.url, value.data);
+    in(error, struct data, value):
+      printf("Processed URL: %s, Error: %s\n", value.url, value.data);
+    end
+    }
+  }
   return 0;
 }
