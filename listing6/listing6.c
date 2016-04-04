e0c5eb8d7d83d94fed625921c78b08502baf0f1b
Amit Saha <amitsaha.in@gmail.com>
2016-04-04 10:56:19 +1000

    Example of mfork()

--- listing6/listing6.c
+++ listing6/listing6.c
@@ -4,16 +4,15 @@
 # include <errno.h>
 # include <unistd.h>
 
-# define NUM_PROCESSES 3
-
 /* Handler coroutine */
 coroutine void handler(tcpsock as) {
-  printf("PID: %d, New connection!\n", getpid());
+  printf("New connection!\n");
   tcpclose(as);
 }
 
 int main(int argc, char **argv)
 {
+  
   int port = 9090;
   ipaddr addr = iplocal(NULL, port, 0);
   tcpsock server = tcplisten(addr, 10);
@@ -21,18 +20,10 @@ int main(int argc, char **argv)
   if (!server) {
     perror("Can't setup a listening server\n");
     return 1;
-  }
-  /* Set up the server processes - the main process is listening as well, so
-     we fork NUM_PROCESSES-1 child processes
-  */
-  for (int i = 1; i < NUM_PROCESSES; i++ ) {
-    pid_t pid = mfork();
-    /* Child process?*/
-    if (pid == 0)
-      break;
+  } else {
+    printf("Server listening on %d\n", port);
   }
 
-  printf("Listening on %d (PID: %d)\n", port, getpid());
   /* Server loop*/
   while(1) {
     tcpsock as = tcpaccept(server, -1);
@@ -42,5 +33,5 @@ int main(int argc, char **argv)
     go(handler(as));
   }
 
-return 0;
+  return 0;
 }
